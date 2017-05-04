79ad5087c9131cb0f900bb8f870dafb5be1b6e47
valentina lopez <valentina lopez>
2017-05-04 13:56:10 -0300

    agregue clase y hidden selected, empecé a crear función de botón

--- js/pokemon.js
+++ js/pokemon.js
@@ -22,12 +22,17 @@ function Pokemon(nombre,color,poderDeAtaque){
 	}
 }
 
-function statement(){
-	var pokemon1 = document.getElementById("fighter1");
-	var pokemon2 = document.getElementById("fighter2")
+
+function ultraViolence(){
+	var data = document.getElementById("caja");
 }
+
+
 var Pikachu = new Pokemon("Pikachu","amarillo", 55)
 var Charmander = new Pokemon("Charmander", "rojo", 52)
+
+
+
 
 var data = document.getElementById("caja");
 //data.innerHTML += Charmander.mostrarPokemon();
