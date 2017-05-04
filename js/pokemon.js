f7181816a9bb581cf3707a78188fa13d8b75ccef
valentina lopez <valentina lopez>
2017-05-04 11:34:20 -0300

    Creados los objetos pokemones, Pikachu y Charmander

---
+++ js/pokemon.js
@@ -0,0 +1,29 @@
+function Pokemon(nombre,color,poderDeAtaque){
+	this.nombre = nombre;
+	this.color = color;
+
+	this.nivelDeAmistad = 0;
+
+	this.vida = 100;
+	this.poderDeAtaque = poderDeAtaque;
+
+	this.mostrarPokemon = function(){
+		return("Hola, soy " + this.nombre + " y soy de color: "+ this.color)
+	}
+
+	this.aumentarAmistad = function(valor){
+		this.nivelDeAmistad = this.nivelDeAmistad + valor
+		return("El nivel de amistad de " + this.nombre + "es de " + this.nivelDeAmistad)
+	}
+
+	this.atacar = function(pokemon){
+		pokemon.vida = pokemon.vida - this.poderDeAtaque
+		return("El ataque de "+ this.pokemon + " disminuyó a: " + pokemon.vida + " la vida de su oponente")
+	}
+}
+var Pikachu = new Pokemon("Pikachu","amarillo", 55)
+var Charmander = new Pokemon("Charmander", "rojo", 52)
+
+var data = document.getElementById("caja");
+data.innerHTML += Charmander.mostrarPokemon();
+//innetHTML es para enviar cosas de js a html
